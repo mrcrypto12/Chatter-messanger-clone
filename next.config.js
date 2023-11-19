5060f9ac5a436d7382b32f7744c22b2a4be9e47c
unknown <lovedanie264@gmail.com>
2023-11-19 09:03:15 +0000

    final

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+        appDir: true,
+        swcPlugins: [
+            ["next-superjson-plugin", {}]
+        ]
+    },
+    images: {
+        domains: [
+            "res.cloudinary.com",
+            "avatars.githubsercontent.com",
+            "lh3.googleusercontent.com"
+        ]
+    }
+}
+
+module.exports = nextConfig
